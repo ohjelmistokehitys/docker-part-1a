2a2bca5a7be88b078ccae67271c80dd0c869ff99
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2024-01-25 17:16:47 +0200

    Fixed test for exercise 1.1

--- .github/classroom/ex11_test.py
+++ .github/classroom/ex11_test.py
@@ -5,9 +5,9 @@ result = sys.stdin.read()
 print(result)
 print("\n" + "=" * 80 + "\n")
 
-assert result.count("Up") >= 2, "There should be two running containers (STATUS Up)"
+assert result.count("Up") >= 1, "There should be one running container (STATUS Up)"
 assert (
-    result.count("Exited") >= 1
-), "There should be an exited container (STATUS Exited)"
+    result.count("Exited") >= 2
+), "There should be two exited containers (STATUS Exited)"
 
 print("Success!")

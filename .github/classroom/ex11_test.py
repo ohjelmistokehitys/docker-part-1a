7923cd18b977d26370df5a8f9455063bcbc6bb24
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2024-01-25 17:18:24 +0200

    Update ex11_test.py

--- .github/classroom/ex11_test.py
+++ .github/classroom/ex11_test.py
@@ -6,8 +6,6 @@ print(result)
 print("\n" + "=" * 80 + "\n")
 
 assert result.count("Up") >= 1, "There should be one running container (STATUS Up)"
-assert (
-    result.count("Exited") >= 2
-), "There should be two exited containers (STATUS Exited)"
+assert result.count("Exited") >= 2, "There should be two exited containers (STATUS Exited)"
 
 print("Success!")
